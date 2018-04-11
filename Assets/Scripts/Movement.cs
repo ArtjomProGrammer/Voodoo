aaef7c31af83d20538acb66b1ad3544aa087c847
Unknown <artjom.schmitke@gmx.de>
2018-04-11 11:58:08 +0200

    Project Voodoo init

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+    public float rotationSpeed;
+    public float moveSpeed = 30;
+
+    private Vector3 oldPosition;
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+
+        Vector3 newPosition = transform.position;
+
+        Vector3 relativePos = newPosition - oldPosition;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = rotation;
+
+        oldPosition = transform.position;
+
+
+
+
+
+
+        //float vAxis = Input.GetAxis("Vertical") * speed;
+        //float hAxis = Input.GetAxis("Horizontal") * speed;
+        //vAxis *= Time.deltaTime;
+        //hAxis *= Time.deltaTime;
+        //transform.Translate(-vAxis, 0, 0);
+        //transform.Translate(0, 0, hAxis);
+        
+        //Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+       
+    }
+
+
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    private const float Y_ANGLE_MIN = 0f;
+    private const float Y_ANGLE_MAX = 50f;
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float currentX = 0f;
+    private float currentY = 0f;
+    private float sensivityX = 4f;
+    private float sensivityY = 1f;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("RotationX");
+        currentY += Input.GetAxis("RotationY");
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(distance, 0, 0);
+        Quaternion rotation = Quaternion.Euler(currentX, 0, currentY);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
