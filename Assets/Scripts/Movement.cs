00af430950aa13b56cfbe8f00c9062a58d7493d8
ArtjomProGrammer <artjom.schmitke@gmx.de>
2018-04-11 17:53:58 +0200

    aktualisiert

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,41 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
     public float speed;
     public float rotationSpeed;
-    public float moveSpeed = 30;
 
-    private Vector3 oldPosition;
+    private float currentX = 0f;
+    private float currentY = 0f;
+
+
 
     void Update()
     {
+        // Player movement
         transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        //transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * 500, 0);
 
-        Vector3 newPosition = transform.position;
+        // Player rotating with camera rotation
+        var CharacterRotation = Camera.main.transform.rotation;
+        CharacterRotation.x = 0;
+        CharacterRotation.z = 0;
 
-        Vector3 relativePos = newPosition - oldPosition;
-        Quaternion rotation = Quaternion.LookRotation(relativePos);
-        transform.rotation = rotation;
+        transform.rotation = CharacterRotation;
 
-        oldPosition = transform.position;
-
-
-
-
-
-
-        //float vAxis = Input.GetAxis("Vertical") * speed;
-        //float hAxis = Input.GetAxis("Horizontal") * speed;
-        //vAxis *= Time.deltaTime;
-        //hAxis *= Time.deltaTime;
-        //transform.Translate(-vAxis, 0, 0);
-        //transform.Translate(0, 0, hAxis);
-        
-        //Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-       
     }
-
-
 }
