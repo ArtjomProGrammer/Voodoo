2d550aa596a71210487553677e035a98c123acc2
ArtjomProGrammer <artjom.schmitke@gmx.de>
2018-07-02 17:00:03 +0200

    rm_3 guards + masks

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,27 +4,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     public float speed;
-    public float rotationSpeed;
-    private float currentX = 0f;
-    private float currentY = 0f;
-
-    public Vector3 rayOffset = new Vector3(0,.05f,0);
     
-    private ThirdPersonCamera cameraInput;
-
-    private void Start()
-    {
-        cameraInput = Camera.main.GetComponent<ThirdPersonCamera>();
-    }
-
-    void Update()
+    void FixedUpdate()
     {
         // Player movement
         transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0,
-                            Input.GetAxis("Vertical") * Time.deltaTime * speed);
-     
+                            Input.GetAxis("Vertical") * Time.deltaTime * speed);   
+    }
+
+    private void LateUpdate()
+    {
         // Player rotating with camera rotation
         var CharacterRotation = Camera.main.transform.rotation;
         CharacterRotation.x = 0;
