2e7dbd2342326b2adc8b35dcc0315bdb6610d14e
FenjaLou <38287501+FenjaLou@users.noreply.github.com>
2018-07-02 17:59:24 +0200

    Merge branch 'master' of https://github.com/ArtjomProGrammer/Voodoo

---
+++ Assets/Scripts/RoomTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour {
+
+    public GameObject Room_02_Trigger;
+    public GameObject Room_03_Trigger;
+    public bool isRoom_02 = true;
+    public bool isRoom_03 = false;
+
+    void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject == Room_02_Trigger)
+        {
+            isRoom_02 = true;
+        }
+
+        if (collision.gameObject == Room_03_Trigger)
+        {
+            isRoom_03 = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject == Room_02_Trigger)
+        {
+            isRoom_02 = false;
+        }
+
+        if (collision.gameObject == Room_03_Trigger)
+        {
+            isRoom_03 = false;
+        }
+    }
+}
