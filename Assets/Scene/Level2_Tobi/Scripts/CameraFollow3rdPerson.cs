5310fca370061a51593770983e333eac515896c5
ConnorVD <tobias.schuster92@gmail.com>
2018-08-03 18:31:36 +0200

    Unity

---
+++ Assets/Scene/Level2_Tobi/Scripts/CameraFollow3rdPerson.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow3rdPerson : MonoBehaviour {
+
+    public float CameraMoveSpeed = 120.0f;
+    public GameObject CameraFollowObj;
+    Vector3 FollowPOS;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject CameraObj;
+    public GameObject PlayerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    private float rotX = 0.0f;
+    private float rotY = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        this.rotX = rot.x;
+        this.rotY = rot.y;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        //this.mouseX = Input.GetAxis("Mouse X");
+        //this.mouseY = Input.GetAxis("Mouse Y");
+        this.finalInputX = inputX + this.mouseX;
+        this.finalInputZ = inputZ + this.mouseY;
+
+        this.rotY += this.finalInputX * this.inputSensitivity * Time.deltaTime;
+        this.rotX += this.finalInputZ * this.inputSensitivity * Time.deltaTime;
+
+        this.rotX = Mathf.Clamp(this.rotX, -this.clampAngle, this.clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(this.rotX, this.rotY, 0.0f);
+        transform.rotation = localRotation;
+
+
+    }
+
+    private void LateUpdate( )
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        // set the target object
+        Transform target = this.CameraFollowObj.transform;
+
+        // move towards the game object that is the target
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Assets/Scene/Level2_Tobi/Scripts/Movement3rdPerson.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement3rdPerson : MonoBehaviour {
+
+    public float speed;
+    private Animator anim;
+
+    public void Start( )
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate( )
+    {
+        // Player movement
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0,
+        //                    Input.GetAxis("Vertical") * Time.deltaTime * speed);
+
+        //if ((Input.GetAxis("Horizontal") > 0f || Input.GetAxis("Vertical") > 0f ||
+        //    Input.GetAxis("Horizontal") < 0f || Input.GetAxis("Vertical") < 0f) &&
+        //    GameObject.Find("Player").GetComponent<PlayerJump>().isGrounded == true)
+        //{
+
+
+        //    anim.SetBool("isWalking", true);
+        //    anim.SetBool("isIdle", false);
+        //    anim.SetBool("isJumping", false);
+        //}
+
+        //if (Input.GetAxis("Horizontal") == 0f && Input.GetAxis("Vertical") == 0f &&
+        //    Input.GetKeyDown(KeyCode.Joystick1Button0) == false)
+        //{
+        //    anim.SetBool("isWalking", false);
+        //    anim.SetBool("isIdle", true);
+        //    anim.SetBool("isJumping", false);
+        //}
+
+       
+
+        float aimX = Input.GetAxisRaw("Horizontal");
+        float aimY = Input.GetAxisRaw("Vertical");
+        float angle = 0.0f;
+
+        if (!(aimX == 0 && aimY == 0))
+        {
+            angle = Mathf.Atan2(aimX, aimY) * Mathf.Rad2Deg;
+            transform.localRotation = Quaternion.Euler(new Vector3(0, angle, 0));
+        }
+
+        this.GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0,
+                            Input.GetAxis("Vertical") * speed);
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0,
+        //                    Input.GetAxis("Vertical") * Time.deltaTime * speed);
+
+    }
+
+    private void LateUpdate( )
+    {
+        //Player rotating with camera rotation
+        //var CharacterRotation = Camera.main.transform.rotation;
+        //CharacterRotation.x = 0;
+        //CharacterRotation.z = 0;
+
+        //transform.rotation = CharacterRotation;
+    }
+}
