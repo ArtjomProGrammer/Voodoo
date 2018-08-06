f06f4bc98d1a1226aeba1c8f9c7b49d08e33e13b
ConnorVD <tobias.schuster92@gmail.com>
2018-08-06 19:27:33 +0200

    Update

--- Assets/Climb.cs
+++ Assets/Climb.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Climb : MonoBehaviour {
 
+    public float jumpTime = 2.05f;
+
     private Animator anim;
+    private bool climb;
+    private float time = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +20,37 @@ public class Climb : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Joystick1Button1))
         {
             anim.SetTrigger("climb");
+            time = 0.0f;
+            this.climb = true;
         }
-	}
+
+
+    }
+
+    private void FixedUpdate( )
+    {
+        if (climb)
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            time += Time.deltaTime;
+            if (time >= 1f && time <= 1.2f)
+            {
+                transform.Translate(0, 0.01f, 0);
+            }
+
+            if (time >= 1.2f && time <= 1.3f)
+            {
+                //transform.Translate(0, 0.01f, 0);
+            }
+
+            if (time >= 2.05f)
+            {
+                //transform.position = new Vector3(transform.position.x, transform.position.y + jumpHeight, transform.position.z);
+                //transform.Translate(0.15f, 0.385f, 0);
+                
+                climb = false;
+                GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
 }
